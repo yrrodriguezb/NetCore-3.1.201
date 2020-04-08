d44cba82d8275128363c7270ad5115f464b102c2
Yeison Rodriguez <yrrodriguezb@gmail.com>
2020-04-08 17:32:36 -0500

    Curso Linq - Introduccion

---
+++ Linq/1. Introduction/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+
+            ShowLargeFilesWithoutLinq(path);
+
+            Console.WriteLine();
+
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path) 
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name, -30} : {file.Length, 10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithLinq(string path) 
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            // or
+            // var query = new DirectoryInfo(path).GetFiles()
+            //    .OrderByDescending(f => f.Length)
+            //    .ToList();
+
+            foreach (FileInfo file in query)
+            {
+                Console.WriteLine($"{file.Name, -30} : {file.Length, 10:N0}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Net Core 3.1 - Lenguaje C# 
+
+### Curso - LINQ Fundamentals - Pluralsight
+
+1. Introudccion
+2. Caracteristicas de C#
+
+    1. Métodos de Extensión
+    2. Expresiones lambda
